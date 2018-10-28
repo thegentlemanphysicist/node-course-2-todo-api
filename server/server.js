93a0b80d4bc526a48acaa6e66d48df931fb7d716
Jon Sharman <jon@freshworks.io>
2018-10-27 19:02:54 -0700

    The server and models

---
+++ server/server.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+let {Todo} = require('./models/todo');
+let {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res)=>{
+  let todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3004, () =>{
+  console.log('Started on port 3004');
+});
