51c8ba004032ce8fc9e2c919a2f096cfa429ead0
Jon Sharman <jon@freshworks.io>
2018-11-13 19:03:35 -0800

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,27 @@ UserSchema.statics.findByToken = function (token) {
   
 };
 
+UserSchema.statics.findByCredentials = function (email,password) {
+  var User = this;
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err,res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+    //return Promise.resolve(user);
+    
+  });
+
+};
+
 UserSchema.pre('save', function (next) {
   let user = this;
   if (user.isModified('password')){
--- server/server.js
+++ server/server.js
@@ -113,11 +113,26 @@ app.post('/users', (req,res) => {
   })
 });
 
-
-
 app.get('/users/me', authenticate, (req,res) => {
   res.send(req.user);
 });
+
+app.post('/users/login', (req,res) =>{
+  var body = _.pick(req.body,['email','password']);
+  
+  User.findByCredentials(body.email,body.password).then((user)=>{
+  //User.findByCredentials(body.email,body.password).then((user)=>{
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+    // res.send(user);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+  
+});
+
+
 
 app.listen(port, () =>{
   console.log(`Started on port ${port}`);
