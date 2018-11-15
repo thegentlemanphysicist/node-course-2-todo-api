71147ee03da43cd21165f5cddb1f5e14ea43f387
Jon Sharman <jon@freshworks.io>
2018-11-14 21:24:21 -0800

    add delete users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,19 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  let user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+      // tokens: {
+      //   token: token
+      // }
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function (token) {
   let User = this;
   let decoded;
